1aea8f57afa2d68d5e2b72143c837be9f9d7deb3
Samson Ugwu <samsonugwu802@gmail.com>
2020-04-05 11:43:13 -1200

    Temperature Low and High

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("can't connect to weather location", undefined)
         } else {
-            console.log(body.daily.data[0])
+    
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The temperature high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + ' chance of rain')
         }
     })
