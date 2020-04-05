94c12d5f22a9cf7c2de7bb4a118fa5f8e7a6418f
Samson Ugwu <samsonugwu802@gmail.com>
2020-04-05 03:28:43 -1200

    added text to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("can't connect to weather location", undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' The temperature is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain')
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The temperature high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + ' chance of rain')
         }
     })
 }
